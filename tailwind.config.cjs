33ddb9920a0b6395db718e74b782f0972921e02d
David Hilera <david.hilera@hotmail.com>
2023-03-02 18:18:23 -0600

    chore: :construction: Add envrioments to tailwind
    Add enviroments to config tailwind, use color from gradients root.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,26 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: ({
+        primary: '#1d9fea',
+        secondary: '#203a85',
+        tertiary: '#ffb885',
+        quaternary: '#ff7e38'
+      }),
+      textColor: {
+        primary: '#1d9fea',
+        secondary: '#203a85',
+        tertiary: '#ffb885',
+        quaternary: '#ff7e38'
+      },
+      ringColor: {
+        primary: '#1d9fea',
+        secondary: '#203a85',
+        tertiary: '#ffb885',
+        quaternary: '#ff7e38'
+      }
+    }
   },
   plugins: []
 }
