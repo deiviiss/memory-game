3c649219000872f381a53bb467692b07a2bd3871
Andrey Calderon <eltercercafe@gmail.com>
2023-03-23 10:41:01 -0500

    feat: :art: Add styles and options to themes

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -6,7 +6,7 @@ function DropDown() {
     const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="relative w-24 flex flex-col items-center rounded-md">
-      <button onClick={() => setIsOpen((prev) => !prev)} className="flex items-center justify-around border-transparent active:border-white duration-400 active:text-white">
+      <button onClick={() => setIsOpen((prev) => !prev)} className="flex items-center justify-around border-transparent hover:border-white duration-400 hover:text-white">
           <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="32" height="32" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 6l16 0"></path><path d="M4 12l16 0"></path><path d="M4 18l16 0"></path></svg>
           {!isOpen ? ( <AiOutlineCaretDown className="h-2" />) : ( <AiOutlineCaretUp className="h-2" />)}
       </button>
