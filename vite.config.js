03ee471f9c677cac5a63c2bad4f9076181deae7a
Andrey Calderon <eltercercafe@gmail.com>
2023-04-03 09:53:53 -0500

    chore: :art: Change to vite.config
    To update the application in my locale

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+        host: 'localhost',
+    },
+    watch: {
+        usePolling: true
+    }
+},
   plugins: [react()]
 })
